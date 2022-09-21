f95b7da9745b28e13431ae4ef2f588a68e852d9f
Chris Key <robertschris010@gmail.com>
2022-09-21 17:23:23 -0500

    port not working

--- server.js
+++ server.js
@@ -8,18 +8,27 @@ const exphbs = require('express-handlebars');
 const hbs = exphbs.create({ helpers });
 
 const session = require('express-session');
-const { appendFileSync } = require('fs');
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
+const sess = {
+    secret: process.env.SESS_SECRET,
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+      db: sequelize
+    }),
+  };
 
 
 
 
-
+app.use(session(sess));
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true }));
@@ -33,5 +42,5 @@ app.use(routes);
 
 //connection to db and server
 sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log(`Now listening on port ${PORT}`));
+    app.listen(PORT, () => console.log('Now listening'));
 });
