38fa71804cc45daded7223803e97563ef9c54ad5
Chris Key <robertschris010@gmail.com>
2022-09-20 19:55:35 -0500

    updated postjs

---
+++ models/Post.js
@@ -0,0 +1,43 @@
+const {Model, DataTypes} = require('sequelize');
+const sequelize = require('../config/connection');
+
+//Creating our post model
+
+class Post extends Model {}
+
+//create columns and fields for post models
+Post.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        content:{
+            type: DataTypes.TEXT,
+            allowNull: true
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+        }
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'post'
+    }
+);
+
+module.exports = Post;
