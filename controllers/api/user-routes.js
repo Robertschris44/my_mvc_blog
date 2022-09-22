b4d70f6b8be9f3802883feb8ea3c30c0e20dcd58
Chris Key <robertschris010@gmail.com>
2022-09-21 23:56:21 -0500

    updated userroutes

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,5 +1,53 @@
 const router = require('express').Router();
-const {User} = require('../../models');
+const {User, Post, Comment} = require('../../models');
+
+
+
+
+//Get api/users
+router.get('/', (req, res) => {
+    User.findAll({
+        attributes: {exclude: ['password']}
+    })
+    .then(dbUserData => res.json(dbUserData))
+    .catch(err => {
+        res.status(500).json(err);
+    });
+});
+
+//Get single user
+router.get('/:id', (req, res) => {
+    User.findOne({
+        attributes: { exclude: ['password']},
+        where: {
+            id: req.params.id
+        },
+        include: [
+            {
+                model: Post,
+                attributes: ['id', 'title', 'post_content', 'created_at']
+            },
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'created_at'],
+                include: {
+                    model: Post,
+                    attributes: ['title']
+                }
+            }
+        ]
+    })
+    .then(dbUserData => {
+        if (!dbUserData) {
+            res.status(404).json({ message: 'No user found with id'});
+            return;
+        }
+        res.json(dbUserData);
+    })
+    .catch(err => {
+        res.status(500).json(err);
+    });
+});
 
 
 //POST
@@ -10,7 +58,7 @@ router.post('/', (req, res) => {
         password: req.body.password,
     }).then(dbUserData => {
         req.session.save(() => {
-            req.session.user_Id = dbUserData.id;
+            req.session.user_id = dbUserData.id;
             req.session.username = dbUserData.username;
             req.session.email = dbUserData.email;
             req.session.password = dbUserData.password;
@@ -24,16 +72,87 @@ router.post('/', (req, res) => {
     });
 });
 
-//LOGIN
+//LOGIN an existing user
 router.post('/login', (req, res) => {
     User.findOne({
         where: {
-            username: req.body.username
+            email: req.body.email
         }
     }).then(dbUserData => {
         if (!dbUserData) {
             res.status(400).json({ message: 'No user found' });
             return;
         }
+
+        const validPassword = dbUserData.checkPassword(req.body.password);
+        if (!validPassword) {
+            res.status(400).json({ messag: 'Incorrect password'});
+            return;
+        }
+
+        req.session.save(() => {
+            req.session.user_id = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.email = dbUserData.email;
+            req.session.password = dbUserData.password;
+            req.session.loggedIn = true;
+
+            res.json({ user: dbUserData, message: 'You are logged in'});
+        });
+    });
+});
+
+//logout
+router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    }
+    else {
+        res.status(404).end();
+    }
+});
+
+//update user
+router.put('/:id', (req, res) => {
+    User.update(req.body, {
+        individualHooks: true,
+        where: {
+            id: req.params.id
+        }
     })
-})
+    .then(dbUserData => {
+        if (!dbUserData[0]) {
+            res.status(400).json({ message: 'no user found'});
+            return;
+        }
+        res.json(dbUserData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+//Delete user
+router.delete('/:id', (req, res) => {
+    User.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbUserData => {
+        if (!dbUserData) {
+            res.status(400).json({ message: 'No user found'});
+            return;
+        }
+        res.json(dbUserData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+module.exports = router;
