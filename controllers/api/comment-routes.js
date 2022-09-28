e4223e0e8ace989bf8990e2777c0133d13c72a45
Chris Key <robertschris010@gmail.com>
2022-09-27 19:32:07 -0500

    Add Heroku

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -1,9 +1,27 @@
 const router = require('express').Router();
-const { Comment } = require('../../models');
+const { User, Post, Comment } = require('../../models');
 const withAuth = require('../../utils/auth');
 
+//These might be causing the error should redo
+
+//Get all comments
 router.get('/', (req, res) => {
-    Comment.findAll({})
+    Comment.findAll({
+        include: [
+            {
+                model:User,
+                attributes: ['username']
+            },
+            {
+                model: Post,
+                attributes: ['id','title','created_at'],
+                include:{
+                    model: User,
+                    attributes: ['id', 'username']
+                }
+            }
+        ]
+    })
     .then(dbCommentData => res.json(dbCommentData))
     .catch(err => {
         console.log(err);
@@ -11,30 +29,31 @@ router.get('/', (req, res) => {
     });
 });
 
-router.post('/', withAuth, (req, res) => {
-    if (req.session) {
-        Comment.create({
-            comment_text: req.body.comment_text,
-            post_id: req.body.post_id,
-            user_id: req.session.user_id,
-        })
-        .then(dbCommentData => res.json (dbCommentData))
-        .catch(err => {
-            console.log(err);
-            res.status(400).json(err);
-        });
-    }
-});
 
-router.delete('/id:', withAuth, (req, res) => {
-    Comment.destroy({
-        where: {
+//Get single comment
+router.get('/:id', (req,res) => {
+    Comment.findOne({
+        where:{
             id: req.params.id
-        }
+        },
+        include: [
+            {
+                model:User,
+                attributes:['username']
+            },
+            {
+                model: Post,
+                attributes:['id', 'title','created_at'],
+                include:{
+                    model:User,
+                    attributes:['id','username']
+                }
+            }
+        ]
     })
     .then(dbCommentData => {
-        if (!dbCommentData) {
-            res.status(404).json({ message: 'no comment found'});
+        if(!dbCommentData){
+            res.status(400).json({message: 'no comment found'});
             return;
         }
         res.json(dbCommentData);
@@ -42,8 +61,44 @@ router.delete('/id:', withAuth, (req, res) => {
     .catch(err => {
         console.log(err);
         res.status(500).json(err);
-    }); 
+    });
 });
 
+//Create a comment
+router.post('/', withAuth, (req,res) => {
+    if(req.session){ //checking the session
+        Comment.create({
+            comment_text:req.body.comment_text,
+            user_id:req.session.user_id,
+            post_id:req.body.post_id
+        })
+        .then(dbCommentData => res.json(dbCommentData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+    };
+});
+
+
+//Delete comment
+router.delete('/;id', withAuth,(req,res) => {
+    Comment.destroy({
+        where:{
+            id: req.params.id
+        }
+    })
+    .then(dbCommentData => {
+        if(!dbCommentData){
+            res.status(400).json({message: 'no comment found'});
+            return;
+        }
+        res.json(dbCommentData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
 
 module.exports = router;
